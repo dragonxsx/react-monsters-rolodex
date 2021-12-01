c040c8c6d9e8b9ff12d5b9f3c8bff7d5e2ff56e0
Long Nguyen <long_nguyen@epam.com>
2021-12-01 16:55:56 +0700

    Dynamic content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { Component } from 'react'; 
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
@@ -7,20 +6,29 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Long'
+      monsters: [
+        {
+          name: 'Frankeistein',
+          id: 'dfd3'
+        },
+        {
+          name: 'Zoombie',
+          id: 'asd3'
+        },
+        {
+          name: 'Dracula',
+          id: '3dsd'
+        }
+      ]
     };
   }
-  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({string: 'Hello Thanh Long'})}>
-            Change text
-          </button>
-        </header>
+        {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
