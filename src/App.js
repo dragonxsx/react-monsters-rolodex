ddf94214cd7f576c6a856df277168214f454a450
Long Nguyen <long_nguyen@epam.com>
2021-12-01 17:42:17 +0700

    Card List Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -13,15 +16,17 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({monsters: users}));
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
+        <CardList>
+          {this.state.monsters.map(monster => (
             <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+          ))}
+        </CardList>
       </div>
     );
   }
